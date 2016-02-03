9ca23790496fca1df16903fb688f7ac6d197ca2f
Patrik Ragnarsson <patrik@starkast.net>
2016-02-03 14:02:14 +0100

    Improve the inspect method
    Remove trailing comma and "@" (a Ruby thing).

--- tests/test_result.py
+++ tests/test_result.py
@@ -12,3 +12,5 @@ class ResultTest(unittest.TestCase):
         self.assertIsInstance(r.number_of_matches_total, int)
         self.assertIsInstance(r.seconds_elapsed, float)
         self.assertEqual(r.all_results_returned(), False)
+        self.assertRegexpMatches(r.__repr__(), "posts, number_of_matches_returned=\d+, number_of_matches_total=\d+")
+        self.assertRegexpMatches(r.__str__(), "Number of matches returned: \d+ Seconds elapsed: \d+.\d{3} Number of matches total: \d+")
