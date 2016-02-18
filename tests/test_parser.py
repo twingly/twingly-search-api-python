fe8407021d982cef853b68e022b7bf12ebe1d5c8
Mattias Roback <mattias.roback@twingly.com>
2016-02-18 14:23:45 +0100

    Remove unused code in Parser tests
    At least I think its not needed. It works in python 2.7 without them.
    Lets see what Travis has to say about it :).

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -31,8 +31,6 @@ class ParserTest(unittest.TestCase):
 
     def test_with_valid_result_containing_non_blogs(self):
         data = self.get_fixture("valid_non_blog_result")
-        if hasattr(data, 'decode'):
-            data = data.decode("utf-8")
         r = twingly_search.Parser().parse(data)
         self.assertIsInstance(r, twingly_search.Result)
         self.assertEqual(len(r.posts), 1)
@@ -40,36 +38,26 @@ class ParserTest(unittest.TestCase):
     def test_with_nonexistent_api_key_result(self):
         with self.assertRaises(twingly_search.TwinglyAuthException):
             data = self.get_fixture("nonexistent_api_key_result")
-            if hasattr(data, 'decode'):
-                data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_unauthorized_api_key_result(self):
         with self.assertRaises(twingly_search.TwinglyAuthException):
             data = self.get_fixture("unauthorized_api_key_result")
-            if hasattr(data, 'decode'):
-                data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_service_unavailable_result(self):
         with self.assertRaises(twingly_search.TwinglyServerException):
             data = self.get_fixture("service_unavailable_result")
-            if hasattr(data, 'decode'):
-                data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_undefined_error_result(self):
         with self.assertRaises(twingly_search.TwinglyServerException):
             data = self.get_fixture("undefined_error_result")
-            if hasattr(data, 'decode'):
-                data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_undefined_error_result(self):
         with self.assertRaises(twingly_search.TwinglyServerException):
             data = self.get_fixture("non_xml_result")
-            if hasattr(data, 'decode'):
-                data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_post_containing_one_tag(self):
