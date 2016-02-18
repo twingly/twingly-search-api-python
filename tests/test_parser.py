f57b45afb2c80850ec669c80e0cb08cd52e6d6ac
Mattias Roback <mattias.roback@twingly.com>
2016-02-18 13:07:42 +0100

    Add fixture helper to ParserTest
    DRY

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,13 +4,18 @@ import unittest
 import twingly_search
 
 class ParserTest(unittest.TestCase):
+    def get_fixture(self, fixture_name):
+        file_path = "./tests/fixtures/%s.xml" % fixture_name
+        fixture = open(file_path, 'r').read()
+        return fixture
+
     def test_with_valid_result(self):
-        data = open("./tests/fixtures/valid_result.xml", 'r').read()
+        data = self.get_fixture("valid_result")
         r = twingly_search.Parser().parse(data)
         self.assertIsInstance(r, twingly_search.Result)
 
     def test_with_valid_result_containing_non_blogs(self):
-        data = open("./tests/fixtures/valid_non_blog_result.xml", 'r').read()
+        data = self.get_fixture("valid_non_blog_result")
         if hasattr(data, 'decode'):
             data = data.decode("utf-8")
         r = twingly_search.Parser().parse(data)
@@ -19,35 +24,35 @@ class ParserTest(unittest.TestCase):
 
     def test_with_nonexistent_api_key_result(self):
         with self.assertRaises(twingly_search.TwinglyAuthException):
-            data = open("./tests/fixtures/nonexistent_api_key_result.xml", 'r').read()
+            data = self.get_fixture("nonexistent_api_key_result")
             if hasattr(data, 'decode'):
                 data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_unauthorized_api_key_result(self):
         with self.assertRaises(twingly_search.TwinglyAuthException):
-            data = open("./tests/fixtures/unauthorized_api_key_result.xml", 'r').read()
+            data = self.get_fixture("unauthorized_api_key_result")
             if hasattr(data, 'decode'):
                 data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_service_unavailable_result(self):
         with self.assertRaises(twingly_search.TwinglyServerException):
-            data = open("./tests/fixtures/service_unavailable_result.xml", 'r').read()
+            data = self.get_fixture("service_unavailable_result")
             if hasattr(data, 'decode'):
                 data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_undefined_error_result(self):
         with self.assertRaises(twingly_search.TwinglyServerException):
-            data = open("./tests/fixtures/undefined_error_result.xml", 'r').read()
+            data = self.get_fixture("undefined_error_result")
             if hasattr(data, 'decode'):
                 data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
 
     def test_with_undefined_error_result(self):
         with self.assertRaises(twingly_search.TwinglyServerException):
-            data = open("./tests/fixtures/non_xml_result.xml", 'r').read()
+            data = self.get_fixture("non_xml_result")
             if hasattr(data, 'decode'):
                 data = data.decode("utf-8")
             r = twingly_search.Parser().parse(data)
