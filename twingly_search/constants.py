225bc7b06458cb5b2fd9e9f7ab503f2079f8bf01
Robin Wallin <robin@twingly.com>
2017-10-16 10:57:29 +0200

    Do not include TZ information in queries
    Same as https://github.com/twingly/twingly-search-api-ruby/pull/69.
    
    Re-record fixtures to get fixtures without published time bugs.

--- twingly_search/constants.py
+++ twingly_search/constants.py
@@ -2,4 +2,5 @@
 from __future__ import unicode_literals
 
 TWINGLY_SEARCH_KEY = 'TWINGLY_SEARCH_KEY'
-DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+POST_DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+QUERY_DATE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
